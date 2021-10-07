eb1d6e4060ce54fa8a7acb3adce6a5c0240ca2f5
JRahul11 <62005970+JRahul11@users.noreply.github.com>
2021-10-07 23:30:31 +0530

    Final Commit

---
+++ lib/model/users.dart
@@ -0,0 +1,11 @@
+class Users {
+  final String imagePath;
+  final String name;
+  final String email;
+
+  const Users({
+    required this.imagePath,
+    required this.name,
+    required this.email,
+  });
+}
